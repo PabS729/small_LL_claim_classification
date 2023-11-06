3e982a51c286e35e98c73ce3185ec087837cec25
Minjing Shi <shimin@eu-login-44.euler.ethz.ch>
2023-11-06 21:26:43 +0100

    s

--- out.sh
+++
@@ -1,3 +0,0 @@
-python train_new.py --model=distilbert-base-uncased --use_silver=0 --use_bronze=0 --epochs=5 --batch_size=16 --seed=22793
-python train_new.py --model=distilbert-base-uncased --use_silver=200 --use_bronze=0 --epochs=5 --batch_size=16 --seed=6048
-python train_new.py --model=distilbert-base-uncased --use_silver=400 --use_bronze=0 --epochs=5 --batch_size=16 --seed=15917
