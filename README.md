fa77372a82a8e95f645f9ea97063f5bd9b9e280b
Minjing Shi <shimin@eu-login-04.euler.ethz.ch>
2023-11-07 11:29:42 +0100

    s

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
-Hierarchy of data folder:
--data
---gold
---silver+bronze
+##Steps to Run
 
-module load gcc/8.2.0 cuda/11.6.2 python_gpu/3.11.2
+1. module load gcc/8.2.0 cuda/11.6.2 python_gpu/3.11.2
 
-pip install -r requirements.txt -U
+2. pip install -r requirements.txt -U
 
-chmod 755 script.sh
+3. chmod 755 script.sh
 
-Insert your wandb key in script.sh
-
-./script.sh
+4. ./script.sh
--- script.sh
+++ script.sh
@@ -1,18 +1,16 @@
 SILVER_LEN=200
-MAX_SILVER_LEN=400
+MAX_SILVER_LEN=4000
 BRONZE_LEN=0
 MAX_BRONZE_LEN=4000
 EPOCHS=5
-BATCHSIZE=16
+BATCHSIZE=32
 
 MODEL='distilbert-base-uncased'
 # echo 'wandb login {api for wandb}' >> out.sh
+module load gcc/8.2.0 cuda/11.6.2 python_gpu/3.11.2
 
-for j in $( eval echo {0..$MAX_SILVER_LEN..$SILVER_LEN} )
+for j in $( eval echo {600..$MAX_SILVER_LEN..$SILVER_LEN} )
 do
     echo "running $j silver labels"
-    echo "python train_new.py --model=$MODEL --use_silver=$j --use_bronze=$BRONZE_LEN --epochs=$EPOCHS --batch_size=$BATCHSIZE --seed=$RANDOM" >> out.sh
+    sbatch --time=30 -G 1 --cpus-per-task=1 --mem-per-cpu=16g --gres=gpumem:8g --wrap="python train_new.py --model=$MODEL --use_silver=$j --use_bronze=$BRONZE_LEN --epochs=$EPOCHS --batch_size=$BATCHSIZE --seed=$RANDOM"
 done
-
-chmod 755 out.sh
-sbatch -G 1 --cpus-per-task=1 --mem-per-cpu=16g --gpus=rtx_2080:1 --wrap='./out.sh' 
